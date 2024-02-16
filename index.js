fa143c7ac87c68f02b353e407ae2e1ffed2c8fe6
parthasarathyAK <sarathypartha631@gmail.com>
2024-02-16 11:02:21 +0530

    interactive greenden project

---
+++ index.js
@@ -0,0 +1,11 @@
+var menu=document.getElementById("menuicon")
+var sidenav=document.getElementById("sidenav")
+var closenav=document.getElementById("closenav")
+
+menu.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
