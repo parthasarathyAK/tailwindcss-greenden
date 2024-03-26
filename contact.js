7e4d870ea5757cf78786467ccc038d0fed84a033
parthasarathyAK <sarathypartha631@gmail.com>
2024-03-26 19:34:12 +0530

    tailwind

--- contact.js
+++ contact.js
@@ -1,15 +1,16 @@
-var menu=document.getElementById("menuicon")
-var sidenav=document.getElementById("sidenav")
-var closenav=document.getElementById("closenav")
 
-menu.addEventListener("click",function(){
+
+
+
+var sidenav = document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+
+menuicon.addEventListener("click",function()
+{
     sidenav.style.right=0
 })
 
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%"
 })
-
-
-
-
